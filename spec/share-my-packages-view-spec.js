b7a641173517400078849437fbd5f952aa21333f
pigjr <pig.junior@gmail.com>
2017-03-07 10:29:46 -0500

    Initial commit
    Feature: copy as plain text

---
+++ spec/share-my-packages-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ShareMyPackagesView from '../lib/share-my-packages-view';
+
+describe('ShareMyPackagesView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
